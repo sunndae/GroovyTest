e8bcfd8caea96f90f41d66aa110f9ad30fe1434e
Tomás Solar <tomassolar478@gmail.com>
2024-11-29 22:42:38 -0300

    add some PDF's to practice programming logic

--- Groovy Essentials/ControlStructures.groovy
+++ Groovy Essentials/ControlStructures.groovy
@@ -3,8 +3,9 @@
 
 // input en groovy por consola
 
-print("Ingresa tu nombre: ")
-nombre = System.in.newReader().readLine()
+//print("Ingresa tu nombre: ")
+//nombre = System.in.newReader().readLine()
+nombre = ""
 
 if (nombre.toLowerCase() == "tomas"){
     println("hola! $nombre")
@@ -14,4 +15,30 @@ if (nombre.toLowerCase() == "tomas"){
     println("No se quien eres :(")
 }
 
+// asi funciona un swictch case en groovy
 
+print("¿Que dia de la semana es?: ")
+String diaDeSemana = System.in.newReader().readLine()
+
+switch (diaDeSemana.toLowerCase()){
+
+    case "lunes":
+        println("Hoy es lunes, toca programar en java para el proyecto")
+        break
+    case "martes":
+        println("Hoy es chill, toca gestión de proyectos")
+        break
+    case "miercoles":
+        println("Hoy es miercoles, toca emilio y visualizacion")
+        break
+    case "jueves":
+        println("Dia libre")
+        break
+    case "viernes":
+        println("Hoy es viernes, toca TAS")
+        break
+
+}
+
+// TODO: hacer un ejercicio que combine todo esto
+// suggest: hacer las UVAS de progra para practica logica de progamacion
