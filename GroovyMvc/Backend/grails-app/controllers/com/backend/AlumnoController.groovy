f73d4c5f39a9de00a1c61a5b35e2fd9101e1f5a5
sunndae <tomassolar478@gmail.com>
2024-12-14 22:13:48 -0300

    Implement service & controller methods

--- GroovyMvc/Backend/grails-app/controllers/com/backend/AlumnoController.groovy
+++ GroovyMvc/Backend/grails-app/controllers/com/backend/AlumnoController.groovy
@@ -1,6 +1,58 @@
 package com.backend
 
-class AlumnoController {
+import grails.web.Controller
 
-    def index() { }
+
+/*
+    El controller sera el que maneje
+    las peticiones HTTP y las respuestas.
+
+*/
+
+// como crear un controller por consola: grails create-controller com.paquete.nombreController
+@Controller
+class AlumnoController {
+    // Definimos una dependencia a un servicio
+    AlumnoService alumnoservice
+
+    def index() {
+        render "esto es un controller para Alumno"
+    }
+
+
+    def findAll(){
+        render status: 200, json: alumnoservice.findAll()
+    }
+
+
+    def findById(Integer id){
+        try {
+            render status: 200, json: alumnoservice.findById(id)
+        } catch (Exception e){
+            render status: 500, text: "Ha ocurrido un error, intente nuevamente. Error: $e"
+        }
+    }
+
+    def createAlumno(Map params){
+        try{
+            alumnoservice.save(Alumno)
+            render status: 201, text: "Alumno creado exitosamente"
+        } catch(Exception e){
+            render status: 500, text: "Un error ha ocurrido, Error: $e"
+        }
+
+    }
+
+    def deleteAlumnoById(Integer id){
+        try {
+            alumnoservice.findById(id)
+            render status: 200, text: "Alumno eliminado correctamente"
+        } catch (Exception e){
+            render status: 500, text: "Ha ocurrido un error, intente nuevamente"
+        }
+    }
+
+    def updateAlumno(Integer id){
+        //todo: terminar el update del controller
+    }
 }
--- GroovyMvc/Backend/grails-app/controllers/com/backend/UrlMappings.groovy
+++ GroovyMvc/Backend/grails-app/controllers/com/backend/UrlMappings.groovy
@@ -1,5 +1,16 @@
 package com.backend
 
+/*
+
+En grails a diferencia de springboot las URLS se definen en esta clase
+
+En este caso, el controlador es "ApplicationController", la acción es "index", y la URL es "/"
+
+Por lo que los metodos que creamos en el service, debemos definir su url para poder probar estos
+metodos posteriormente en postman o en la vista que creemos posteriormente.
+
+*/
+// todo: implementar los path de alumno
 class UrlMappings {
     static mappings = {
         "/$controller/$action?/$id?(.$format)?"{
