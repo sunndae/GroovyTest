7d9ef49ba30a2d90fb47eab09049ce7e5356a255
sunndae <tomassolar478@gmail.com>
2024-12-25 18:40:07 -0300

    Implement CRUD methos for Department domain class

--- GroovyMvc/Backend/bin/main/com/backend/DepartamentoService.groovy
+++ GroovyMvc/Backend/bin/main/com/backend/DepartamentoService.groovy
@@ -2,10 +2,63 @@ package com.backend
 
 import groovy.transform.CompileStatic
 
+import grails.gorm.transactions.Transactional
+
 @CompileStatic
+@Transactional
 class DepartamentoService {
 
-    def doSomething() {
+    def findByid(Integer id){
+        Departamento departamento = Departamento.get(id)
+        if(!Departamento){
+            throw new RuntimeException("Departamento no encontrado")
+        }
+        else{
+            return departamento
+        }
+    }
 
+
+    def findAll(){
+        try{
+            Departamento departamento = Departamento.list()
+            return departamento
+        }catch(Exception e){
+            throw new RuntimeException("Error al obtener los departamentos de la base de datos" + e)
+    }
+
+    def createDepartamento(Map params){
+        Departamento nuevoDepartamento = new Departamento(params)
+        try{
+            nuevoDepartamento.save(flush: true)
+            return nuevoDepartamento
+        }catch(Exception e){
+            throw new RuntimeException("Ha ocurrido un error al crear un departamento" + e)
+    }
+
+    def deleteDepartamentoById(Integer id){
+        Departamento departamento = Departamento.get(id)
+        if(!Departamento){
+            throw new RuntimeException("El departamento no existe en la base de datos")
+        } else{
+            return departamento.delete(flush:true)}
+    }
+
+    @Transactional
+    def updateDepartamento(Integer id, Map params){
+        Departamento departamentoActualizar = Departamento.get(id)
+        if(!departamentoActualizar){
+            throw new RuntimeException("El departamento no existe en la base de datos")
+        }
+        try{
+            departamentoActualizar.properties = params
+
+            if(!departamentoActualizar.save(flush:true)){
+                throw new RuntimeException("Ha ocurrido un error al actualizar el departamento")
+            }
+            return departamentoActualizar
+        }catch(Exception e){
+            throw new RuntimeException("Ha ocurrido un error al actualizar el departamento" + e)
+        }
     }
 }
--- GroovyMvc/Backend/grails-app/domain/com/backend/Departamento.groovy
+++ GroovyMvc/Backend/grails-app/domain/com/backend/Departamento.groovy
@@ -2,6 +2,17 @@ package com.backend
 
 class Departamento {
 
+    String name
+    Integer phoneNumber
+
+    
     static constraints = {
+        name nullable: false
+        phoneNumber nullable: false
+    }
+
+    static mapping = {
+        table: 'departamento'
+        version: false
     }
 }
